f69b32af280cbacb7cf798a50186f93237f992d0
Hengers3012 <hengersrosario.m@gmail.com>
2023-03-28 15:38:05 -0400

    Actualizando mi App - Mejorando el flujo del programa

--- rick_and_morty/src/components/Nav/Nav.jsx
+++ rick_and_morty/src/components/Nav/Nav.jsx
@@ -1,20 +1,3 @@
-// import React from "react";
-// import SearchBar from "../SearchBar/SearchBar";
-// import styles from "./Nav.module.css";
-// // import NavLogo from "../../Image/Logo.png";
-
-// export default function Nav(props) {
-//   return (
-//     <div className={styles.navContenedor}>
-//       <img className="logo" src="" alt="" />
-
-//       <div className="Search">
-//         <SearchBar onSearch={(characterID) => props.onSearch(characterID)} />
-//       </div>
-//     </div>
-//   );
-// }
-
 import React from "react";
 import SearchBar from "../SearchBar/SearchBar.jsx";
 import styles from "./Nav.module.css";
