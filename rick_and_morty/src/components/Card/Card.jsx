05388e74d1713d608304c7b10c0c374a110958d6
Hengers3012 <hengersrosario.m@gmail.com>
2023-03-28 14:42:34 -0400

    Actualizando mi App - Mejorando el flujo del programa

--- rick_and_morty/src/components/Card/Card.jsx
+++ rick_and_morty/src/components/Card/Card.jsx
@@ -3,22 +3,22 @@ import { Link } from "react-router-dom";
 
 export default function Card({ id, name, species, image, gender, onClose }) {
   return (
-    <Link to={`/detail/${id}`} className={styles.link}>
-      <div className={styles.contenedor}>
-        <div className={styles.contenedorBtnX}>
-          <button className={styles.buttonX} onClick={onClose}>
-            X
-          </button>
-        </div>
+    <div className={styles.contenedor}>
+      <div className={styles.contenedorBtnX}>
+        <button className={styles.buttonX} onClick={onClose}>
+          X
+        </button>
+      </div>
+      <Link to={`/detail/${id}`} className={styles.link}>
         <div className={styles.contenedorImage}>
           <h2 className={styles.namePersonaje}>{name}</h2>
           <img src={image} alt="" />
         </div>
-        <div className={styles.propsDContenedor}>
-          <h2>{species}</h2>
-          <h2>{gender}</h2>
-        </div>
-      </div>
-    </Link>
+      </Link>
+      <div className={styles.propsDContenedor}>
+        <h2>{species}</h2>
+        <h2>{gender}</h2>
+      </div>{" "}
+    </div>
   );
 }
