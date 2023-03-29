db5a539c835e3d2c058218acdf66016e4e8aeab8
Hengers3012 <hengersrosario.m@gmail.com>
2023-03-29 17:52:16 -0400

    Actualizando mi App - Mejorando el flujo del programa

--- rick_and_morty/src/components/Nav/Nav.jsx
+++ rick_and_morty/src/components/Nav/Nav.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "../SearchBar/SearchBar.jsx";
 import styles from "./Nav.module.css";
 import { NavLink } from "react-router-dom";
+// import { Logo } from "../../Image/Logo.png";
 const NavLinkMe = ({ to, children, ...props }) => {
   return (
     <NavLink
@@ -20,6 +21,13 @@ export default function Nav(props) {
 
   return (
     <div className={styles.navContenedor}>
+      <div className={styles.img}>
+        <img
+          src="https://hbomax-images.warnermediacdn.com/images/GXkRjxwjR68PDwwEAABKJ/logoburnedinleft?size=600x181&format=png&partner=hbomaxcom&host=art-gallery.api.hbo.com&language=es-es&w=600"
+          alt=""
+        />
+      </div>
+
       <NavLinkMe to="/home">Home</NavLinkMe>
       <NavLinkMe to="/about">About</NavLinkMe>
       <NavLinkMe to="/favorites">Favorites</NavLinkMe>
