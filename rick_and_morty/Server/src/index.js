c3da94d6670286a810a1710d47574e2749f40611
Hengers3012 <hengersrosario.m@gmail.com>
2023-04-21 15:15:41 -0400

    Actualizando (Back End)(Front) - Add Web Server

---
+++ rick_and_morty/Server/src/index.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const data = require("./utils/data");
+const PORT = 3001;
+http
+  .createServer((req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    console.log(req.url);
+    try {
+      const { url } = req;
+      if (url.includes("/rickandmorty/character")) {
+        const id = url.split("/").at(-1);
+        console.log(id);
+
+        const character = data.find((charact) => charact.id == id);
+        console.log(character);
+
+        if (character) {
+          res.writeHead(200, { "Content-Type": "application/json" });
+
+          res.end(JSON.stringify(character));
+        } else {
+          res.writeHead(404, { "Content-Type": "application/json" });
+
+          res.end(
+            JSON.stringify({ error: `Character not found with id: ${id}` })
+          );
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
+    }
+  })
+  .listen(PORT, () => {
+    console.log(`Server on port ${PORT}`);
+  });
