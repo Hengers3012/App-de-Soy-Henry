c17f593fc37237ceaf533d57ad109d64fc291c1e
Hengers3012 <hengersrosario.m@gmail.com>
2023-05-03 14:03:23 -0400

    Update Integration

--- rick_and_morty/Server/src/routes/index.js
+++ rick_and_morty/Server/src/routes/index.js
@@ -1,3 +1,13 @@
 const express = require("express");
+const router = express.Router();
 
-const { postFav, deleteFav } = require("../controllers/handleFavorites");
+const { login } = require("../controllers/login.js");
+const { getCharById } = require("../controllers/getCharById.js");
+const { postFav, deleteFav } = require("../controllers/handleFavorites.js");
+
+router.get("/character/ :id", getCharById);
+router.get("/login", login);
+router.post("/favElement", postFav);
+router.delete("/favElement", deleteFav);
+
+module.exports = router;
