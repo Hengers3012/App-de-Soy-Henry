f2714f14a8a143792b46301de5b93693d25f8387
Hengers3012 <hengersrosario.m@gmail.com>
2023-05-04 09:19:59 -0400

    Update

--- rick_and_morty/Server/src/routes/index.js
+++ rick_and_morty/Server/src/routes/index.js
@@ -11,8 +11,8 @@ const {
 } = require("../controllers/index");
 
 router.get("/character/:id", getCharById);
-// router.get("/login", login);
-// router.post("/favElement", postFav);
-// router.delete("/favElement/:id", deleteFav);
+router.get("/login", login);
+router.post("/favElement", postFav);
+router.delete("/favElement/:id", deleteFav);
 
 module.exports = router;
